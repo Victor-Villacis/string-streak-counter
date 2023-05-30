ba25c6201df4606b742b5ed7dc2dffeec9780e15
Victor Villacis <victor.villacis@outlook.com>
2023-05-30 04:34:25 -0400

    just wanted to try and catch something

--- client/src/services/TodoService.ts
+++ client/src/services/TodoService.ts
@@ -1,27 +1,45 @@
-// TodoService.ts
 import axios from 'axios';
-
 
 export default class TodoService {
     static async getTodos() {
-        const response = await axios.get(`/api/all-todos`);
-        console.log(response.data);
-        return response.data;
+        try {
+            const response = await axios.get(`/api/all-todos`);
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            console.error('Error retrieving TODO:', error);
+            throw error;
+        }
     };
 
     static async createTodo(todo: any) {
-        const response = await axios.post(`/api/new-todo`, todo);
-        return response.data;
+        try {
+            const response = await axios.post(`/api/new-todo`, todo);
+            return response.data;
+        } catch (error) {
+            console.error('Error creating TODO:', error);
+            throw error;
+        }
     };
 
     static async updateTodo(updatedTodo: any) {
-        console.log(updatedTodo, 'in updateTodo');
-        const response = await axios.put(`/api/update-todo/${updatedTodo.id}`, updatedTodo);
-        return response.data;
+        try {
+            console.log(updatedTodo, 'in updateTodo');
+            const response = await axios.put(`/api/update-todo/${updatedTodo.id}`, updatedTodo);
+            return response.data;
+        } catch (error) {
+            console.error('Error updating TODO:', error);
+            throw error;
+        }
     };
 
     static async deleteTodo(id: string) {
-        const response = await axios.delete(`/api/delete-todo/${id}`);
-        return response.data;
+        try {
+            const response = await axios.delete(`/api/delete-todo/${id}`);
+            return response.data;
+        } catch (error) {
+            console.error('Error deleting TODO:', error);
+            throw error;
+        }
     };
 }
